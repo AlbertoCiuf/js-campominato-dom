7b0694c08038ba2493f74bfea04eab518820f8e4
AlbertoCiuf <alberton.ciufici@gmail.com>
2021-11-03 12:15:47 +0100

    generazione bombe con controllo doppioni

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const playButton = document.getElementById('play-button');
 const difficulty = document.getElementById('difficulty');
 let chosenDifficulty;
 
+const bombs = [];
+const BOMBS_NUMBER = 16;
 
 
 // funzione che al click del pulsante "Play!" genera le griglie in base alla difficoltà scelta
@@ -17,18 +19,34 @@ playButton.addEventListener('click', function() {
       const square = generateSquare(grid);
       square.innerHTML = i+1;
     }
+    for (let i=0; i<BOMBS_NUMBER; i++) {
+      let bomb = getRandomInt(1,100);
+      if (!bombs.includes(bomb)) bombs.push(bomb);
+      else i--;
+   }
 
   } else if (chosenDifficulty===2) {
     for (let i=0; i<81; i++) {
       const square = generateSquare(grid);
       square.innerHTML = i+1;
     }
-
+    for (let i=0; i<BOMBS_NUMBER; i++) {
+      let bomb = getRandomInt(1,81);
+      if (!bombs.includes(bomb)) bombs.push(bomb);
+      else i--;
+   }
   } else {
     for (let i=0; i<49; i++) {
       const square = generateSquare(grid);
       square.innerHTML = i+1;
     }
+    for (let i=0; i<BOMBS_NUMBER; i++) {
+      let bomb = getRandomInt(1,49);
+      if (!bombs.includes(bomb)) bombs.push(bomb);
+      else i--;
+   }
+   console.log(bombs);
+
   }
 
 });
@@ -42,11 +60,15 @@ function generateSquare(target) {
     sq.classList.add('easy');
   } else if (chosenDifficulty === 2) {
     sq.classList.add('normal');
-  } else sq.classList.add('hard');
+  } else {
+    sq.classList.add('hard');
+
+  }
 
   //onClick delle celle
-  sq.addEventListener('click', function(){
+  sq.addEventListener('click', function(event){
     this.classList.add('clicked');
+    //console.log(event.target.innerText);
   });
 
 
@@ -59,3 +81,5 @@ function generateSquare(target) {
 function getRandomInt(min,max) {
   return Math.floor(Math.random()*(max - min + 1) + min);
 }
+
+
